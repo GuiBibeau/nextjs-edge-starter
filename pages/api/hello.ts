d4e1f84f6a513604fe445e698fc21781c097fee1
GuiBibeau <g.bibeaulaviolette@gmail.com>
2022-09-24 23:34:51 -0400

    edge api routes

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,13 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextRequest } from "next/server";
 
-type Data = {
-  name: string
+export default async function handler(req: NextRequest) {
+  // Contrary to regular Next.js API routes, we use Standard Web APIs
+  const { searchParams } = new URL(req.url);
+  const name = searchParams.get("name");
+  return new Response(`Hello ${name ?? "World"}!`);
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
-}
+// this will specify to the Vercel build system to deply to the edge network!
+export const config = {
+  runtime: "experimental-edge",
+};
